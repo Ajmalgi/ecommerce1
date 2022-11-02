6ddae3c3e135127d34a11231c1a2010b86f2efdc
Ajmal# <ajmalomr5@gmail.com>
2022-11-02 13:06:47 +0530

    ecomm

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&rj!9lm6thwiqbjqo!ybtfeh862s0ldoznai(+b8m_=cwhs%ej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
